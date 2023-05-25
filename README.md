73833226c6f35afe4e5771667840520e70da2aac
Nils <nilsbaumgartner1994@gmail.com>
2023-05-25 02:11:55 +0200

    Extracted from data-clumps

---
+++ README.md
@@ -0,0 +1,18 @@
+<h2 align="center">
+    data-clumps-type-context
+</h2>
+
+## About
+
+A reporting format for Data-Clumps as used in [data-clumps](https://github.com/FireboltCasters/data-clumps/).
+
+
+## License
+
+MIT
+
+## Contributors
+
+The FireboltCasters
+
+<a href="https://github.com/FireboltCasters/data-clumps-type-context"><img src="https://contrib.rocks/image?repo=FireboltCasters/data-clumps-type-context" alt="Contributors" /></a>
---
+++ src/api/src/index.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+import {DataClumpsTypeContext as DataClumpsTypeContext_} from "./ignoreCoverage/DataClumpTypes";
+import {DataClumpTypeContext as DataClumpTypeContext_}  from "./ignoreCoverage/DataClumpTypes";
+import {DataClumpsParameterToContext as DataClumpsParameterToContext_}   from "./ignoreCoverage/DataClumpTypes";
+import {DataClumpsParameterFromContext as DataClumpsParameterFromContext_} from "./ignoreCoverage/DataClumpTypes";
+import {Dictionary as Dictionary_} from "./ignoreCoverage/DataClumpTypes";
+
+export type DataClumpsTypeContext = DataClumpsTypeContext_; // https://stackoverflow.com/questions/53728230/cannot-re-export-a-type-when-using-the-isolatedmodules-with-ts-3-2-2
+export type DataClumpTypeContext = DataClumpTypeContext_; // https://stackoverflow.com/questions/53728230/cannot-re-export-a-type-when-using-the-isolatedmodules-with-ts-3-2-2
+export type DataClumpsParameterToContext = DataClumpsParameterToContext_; // https://stackoverflow.com/questions/53728230/cannot-re-export-a-type-when-using-the-isolatedmodules-with-ts-3-2-2
+export type DataClumpsParameterFromContext = DataClumpsParameterFromContext_; // https://stackoverflow.com/questions/53728230/cannot-re-export-a-type-when-using-the-isolatedmodules-with-ts-3-2-2
+export type Dictionary<T> = Dictionary_<T>; // https://stackoverflow.com/questions/53728230/cannot-re-export-a-type-when-using-the-isolatedmodules-with-ts-3-2-2
